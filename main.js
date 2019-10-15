fefdc98e1383e4bec0c724471472b10b932e6c4b
H. Nicolás Caffarini <ncaffarini@gmail.com>
2019-10-15 20:11:57 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const { app, BrowserWindow, globalShortcut } = electron;
+
+let ventana;
+
+app.on('ready', () => {
+	ventana = new BrowserWindow();
+
+	ventana.setMenu(null);
+	ventana.setTitle("ITIC");
+	ventana.loadURL('https://economicasuba.sharepoint.com/sites/Sistemas/AULA-208026/');
+
+	ventana.on('closed', () => {
+		ventana = null;
+	});
+});
